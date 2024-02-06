0e10ec4a5714b552291f5ceff49572717121d18a
aleksei3479 <138991048+aleksei3479@users.noreply.github.com>
2024-02-07 01:06:47 +0300

    домашка

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1;
+        int age = 30;
+        if (age >= 18) ;
+        if (age <= 17) ;
+        System.out.println("Если возвраст человека" + age + "он совершенолетний");
+        System.out.println("Если возвраст человека" + age + "он не недостиг совершенолетия, нужно немного подождать");
+        // Задача 2;
+        int temperature = 5;
+        if (temperature <= 5) ;
+        System.out.println("На улице холодно" + temperature + "нужно надеть шапку");
+        if (temperature >= 5) ;
+        System.out.println("Сегодня тепло" + temperature + "можно идти без шапки");
+        // Задача 3;
+        int speed = 60;
+        if (speed <= 60) ;
+        if (speed >= 60) ;
+        System.out.println("Скорость превышена" + speed + "если она больше");
+        System.out.println("Превышение скорости нет" + speed + "если она меньше");
+        // Задача 4;
+        int agePercon = 15;
+        if (agePercon <= 2) ;
+        if (agePercon >= 2 && agePercon <= 6) ;
+        if (agePercon >= 7 && agePercon <= 17) ;
+        if (agePercon >= 18 && agePercon <= 24) ;
+        if (agePercon >= 24 && agePercon <= 30) ;
+        System.out.println("Если возраст человека" + agePercon + "то ему нужно ходить в детский сад");
+        System.out.println("Если возраст человека" + agePercon + "то ему нужно ходить в школу");
+        System.out.println("Если возраст челавека" + agePercon + "то ему нужно ходить в университет");
+        System.out.println("Если возраст челавека" + agePercon + "то ему нужно ходить на работу");
+        // Задача 5;
+        int years = 3;
+        if (years <= 5) ;
+        if (years >= 5 && years <= 14) ;
+        if (years >= 14 && years <= 16) ;
+        System.out.println("В зависимости от возраста" + years + "нельзя кататся на атракционе");
+        System.out.println("В зависимости от возраста" + years + "можно кататся на атракционе в  сопроваждение взрослых");
+        System.out.println("В зависимости от возраста" + years + "можно кататся на атракционе без сопроваждения взрослых");
+        // Задача 6;
+        int railwaycarriage = 102;
+        if (railwaycarriage <= 60) ;
+        if (railwaycarriage >= 60 && railwaycarriage <= 102) ;
+        System.out.println("Вагон расчитан" + railwaycarriage + "на сидячие места");
+        System.out.println("Вагон расчитан" + railwaycarriage + "все остальные стоячие");
+
+
+
+
+
+
+
+
+
+
+    }
+    }
